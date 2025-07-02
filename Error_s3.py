5269ad66d5c52fa6f9782f1694095641f620a3e5
Steven Dung Le <46384935+Dungle93@users.noreply.github.com>
2025-07-02 10:15:16 +0700

    Create Error_s3.py

---
+++ Error_s3.py
@@ -0,0 +1,17 @@
+import boto3
+import pandas as pd
+
+def process_data(bucket, key):
+  '''
+  This function works as follows:
+  Read file from S3 -> filter data: amount /1000 -> write back to S3
+  '''
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    df = pd.read_csv(obj['Body'])
+
+    filtered = df[df['amount'] > 1000]
+
+    s3.put_object(Bucket=bucket, Key='filtered_data.csv', Body=filtered.to_csv(index=False))
+
+process_data('my-bucket', 'data/sales.csv')
