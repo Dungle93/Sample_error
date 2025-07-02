53881951503148af32707e0dfd247a53378232b0
Steven Dung Le <46384935+Dungle93@users.noreply.github.com>
2025-07-02 10:36:31 +0700

    Update Error_etl_BigQuery.py

--- Error_etl_BigQuery.py
+++ Error_etl_BigQuery.py
@@ -3,13 +3,6 @@ import pandas as pd
 from google.cloud import bigquery
 
 def etl():
-    '''
-    This function works as follows:
-    1. Connect Postgres
-    2. Access Transaction table 
-    3. convert currency
-    4. push to Bigquery
-    '''
     engine = create_engine("postgresql://user:pass@host:5432/dbname")
     df = pd.read_sql("SELECT * FROM transactions", engine)
     df['amount_usd'] = df['amount'] * 23000  # Convert VND to USD
