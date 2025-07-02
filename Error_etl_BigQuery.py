85281db121f84bcab9ef31e3d489fa077cd5cb6d
Steven Dung Le <46384935+Dungle93@users.noreply.github.com>
2025-07-02 10:09:27 +0700

    Create Error_etl_BigQuery.py

---
+++ Error_etl_BigQuery.py
@@ -0,0 +1,17 @@
+
+def etl():
+    '''
+    This function works as follows:
+    1. Connect Postgres
+    2. Access Transaction table 
+    3. convert currency
+    4. push to Bigquery
+    '''
+    engine = create_engine("postgresql://user:pass@host:5432/dbname")
+    df = pd.read_sql("SELECT * FROM transactions", engine)
+    df['amount_usd'] = df['amount'] * 23000  # Convert VND to USD
+
+    client = bigquery.Client()
+    client.load_table_from_dataframe(df, "project.dataset.table")
+
+etl()
